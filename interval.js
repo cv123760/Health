d7df6f78f026243f8e3952ccda983911bdb7f723
cv123760 <77294770+cv123760@users.noreply.github.com>
2021-01-23 07:47:50 -0700

    Add files via upload

--- interval.js
+++ interval.js
@@ -3,7 +3,20 @@ function main(){
 var distance = document.getElementById("distance").value;
 var time = document.getElementById("time").value;
 var sessionDuration = document.getElementById("sessionDuration").value;
-var intervalLength = document.getElementById("intervalLength").value;
+var intervalLength = document.getElementById("intervalLength").value
+ 
+if(distance <= 0){
+    console.log("null ")
+    document.getElementById("fastInterval").innerHTML = ("Please enter values greater than zero");
+    document.getElementById("runFast").innerHTML = "";
+    document.getElementById("slowInterval").innerHTML = "";
+    document.getElementById("runSlow").innerHTML = "";
+    document.getElementById("total").innerHTML = "";
+
+    return
+
+    
+}
 
 var pace = time/distance;   // determines comfortable pace
 var fastTime = pace/4 - 4/60 //caculates fast interval Time
@@ -51,10 +64,11 @@ slowerTime =getSeconds(slowerTime);
 
 console.log(fastTime, fastPace, ti)
 
+
 document.getElementById("fastInterval").innerHTML = ("Fast Interval");
-document.getElementById("runFast").innerHTML = "Run " + intervalLength + " mile under " + fastTime + "(min:sec) at a pace of " + fastPace + "min/mi";
+document.getElementById("runFast").innerHTML = "Run " + intervalLength + " miles in under " + fastTime + "(min:sec) at a pace of " + fastPace + " (min/mi)";
 document.getElementById("slowInterval").innerHTML = "Slow Interval";
-document.getElementById("runSlow").innerHTML = "Run " + intervalLength + " between " + slowTime + " and " + slowerTime + " at a pace between " + slowPace + " and " + slowerPace;
-document.getElementById("total").innerHTML = "run a total of " + ti + " intervals";
+document.getElementById("runSlow").innerHTML = "Run " + intervalLength + " miles between " + slowTime + " and " + slowerTime + " (min:sec) at a pace between " + slowPace + " and " + slowerPace + " (min/mi).";
+document.getElementById("total").innerHTML = "Run a total of " + ti + " intervals";
 
 }
