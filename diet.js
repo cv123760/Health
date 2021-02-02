d290f69f7d2233fee77429cc80874efa52ccd0ac
Carlos <cv123760@gmail.com>
2021-02-02 12:39:06 -0700

    updated javascript

--- diet.js
+++ diet.js
@@ -1,6 +1,6 @@
-const main = () => { 
- 
- class meal {
+
+function main(){ 
+    class meal {
 
         constructor(protein, carb, veggie){
         this.protein = protein;
@@ -12,22 +12,52 @@ const main = () => {
             console.log("${this.protein}, ${this.veggie}, ${this.carb}");
         }
     }
+ 
+    function getag(input){
+    let a = document.querySelector(input).value
+    return a
+    }
 
 
-    const getag = (num) =>  document.getElementsBysTagName("input[num]").value
+    let p = getag(".protein");
+    let c = getag(".carb");
+    let v = getag(".veggie");
 
-    let p = getag(0);
-    let c = getag(1);
-    let v = getag(2);
     console.log(p, c, v)
 
-    let breakfast = new meal(p, c, v);
+    let newMeal = new meal(p, c, v);
+    console.log("protein is " ,newMeal.protein)
 
-
-    var meal = {
-        protein: p
+    function createE(text){
+        let p = document.createElement("p")
+        let n = document.createTextNode(text)
+        p.appendChild(n)
+        return p
+    }   
+    
+    function createTd(p,v,c){
+        let td = document.createElement("td");
+        td.appendChild(p);
+        td.appendChild(v)
+        td.appendChild(c)
+        console.log(td, "before return")
+        return td
     }
-}
+    
+    let pp = createE(newMeal.protein);
+    let pv = createE(newMeal.veggie);
+    let pc = createE(newMeal.carb);
 
-const button = () => gettag("button") 
-button.addEventListener(KeyboardEvent, main)
+    let td = createTd(pp, pv, pc);
+    console.log(td)
+    let app = document.querySelector(".breakFast");
+    app.appendChild(td)
+    
+
+
+
+    
+
+    
+
+}
