309178451db87f2a764fb085e069c5f29570e732
Carlos Valenzuela <cv123760@gmail.com>
2021-09-25 18:46:29 -0700

    fix bugs

---
+++ public/Foods.js
@@ -0,0 +1,35 @@
+{
+    Protein:[
+        '1% Lean Ground Turkey',
+        'Chicken Breast',
+        'Egg Whites',
+        'Lean Beef', 
+        'Turkey Breasts',
+        'White Fish'
+    ],
+
+    Carb:[
+        'Acai Berries',
+        'Black Berries',
+        'Black Rice',
+        'Blue Berries',
+        'Brown Rice',
+        'Cous Cous',
+        'Mango',
+        'Oatmeal **',
+        'Pineapple',
+        'Quinoa',
+        'Red Berries',
+        'Starfruit',
+        'Sweet Potato'
+    ],
+    Veggie:[
+        'Asparragus',
+        'Broccoli',
+        'Brussel Sprouts',
+        'Cucumber',
+        'Kale',
+        'Spinach',
+        'Spring Mix'
+    ],
+}
