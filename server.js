4397a9ed1377bbaa571a5bf46a42daf39628e2b0
Carlos Valenzuela <cv123760@gmail.com>
2021-10-09 14:31:41 -0700

    fix url

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const findOrCreate = require("mongoose-findorcreate");
 
 const app = express();
 
-const url = "https://floating-hamlet-55199.herokuapp.com"
+const url = "floating-hamlet-55199.herokuapp.com"
 
 
 app.use(express.static(__dirname+"/public"))
